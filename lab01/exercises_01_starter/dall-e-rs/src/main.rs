7d0169c70a2b8931d3a563fa7e1c6e34d64a39c9
Benji Schwartz <benji.schwartz2013@gmail.com>
2024-02-20 14:08:21 +1100

    finished dall-e exo

--- lab01/exercises_01_starter/dall-e-rs/src/main.rs
+++ lab01/exercises_01_starter/dall-e-rs/src/main.rs
@@ -12,21 +12,41 @@ fn main() {
 
     for x in 0..IMAGE_SIZE_PX {
         for y in 0..IMAGE_SIZE_PX {
-            if (x + y) % 15 == 0 {
-                img.set_pixel(x, y, red);
-            } else if (x + y) % 15 == 5 {
-                img.set_pixel(x, y, green);
-            } else if (x + y) % 15 == 10 {
-                img.set_pixel(x, y, blue);
-            } else {
-                img.set_pixel(x, y, black);
-            }
+            img.set_pixel(x, y, create_pixel(x as u8, y as u8, ((x + y) % 256) as u8));
         }
     }
+
+    let img = draw_circle(30, (IMAGE_SIZE_PX/2, IMAGE_SIZE_PX/2), img, green);
+    let img = draw_circle(7, (IMAGE_SIZE_PX/2 + 10, IMAGE_SIZE_PX/2 - 10), img, black);
+    let img = draw_circle(7, (IMAGE_SIZE_PX/2 - 10, IMAGE_SIZE_PX/2 - 10), img, black);
+    let img = draw_circle(4, (IMAGE_SIZE_PX/2, IMAGE_SIZE_PX/2 + 10), img, black);
+
     img.save("my_image.bmp")
         .expect("Failed to save image!");
 }
 
 fn create_pixel(r: u8, g: u8, b: u8) -> Pixel {
     return Pixel::new(r, g, b);
+}
+
+fn draw_circle(radius: u32, centre: (u32, u32), mut img: bmp::Image, colour: Pixel) -> bmp::Image {
+
+    for x in 0..IMAGE_SIZE_PX {
+        for y in 0..IMAGE_SIZE_PX {
+            if within_radius(radius, centre, (x, y)) {
+                img.set_pixel(x, y, colour);
+            }
+        }
+    }
+    img
+}
+fn within_radius(radius: u32, centre: (u32, u32), coords: (u32, u32)) -> bool {
+    let x_dist = centre.0 as i64 - coords.0 as i64;
+    let y_dist = centre.1 as i64 - coords.1 as i64;
+    let distance_squared = x_dist.pow(2) as f64 + y_dist.pow(2) as f64;
+    let distance = distance_squared.sqrt() as f64;
+    if distance <= radius as f64 {
+        return true;
+    }
+    return false;
 }
