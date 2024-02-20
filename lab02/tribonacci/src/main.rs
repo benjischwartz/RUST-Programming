8b6a972ca36e043d932560b55491485947ba8a6c
Benji Schwartz <benji.schwartz2013@gmail.com>
2024-02-20 16:04:52 +1100

    finished tribonacci

--- lab02/tribonacci/src/main.rs
+++ lab02/tribonacci/src/main.rs
@@ -25,6 +25,22 @@ fn compute_tribonacci(
     // inside the `TribonacciError` struct
     error_msg: String,
 ) -> Result<(), TribonacciError> {
-    // TODO: complete this function!
-    Ok(())
+    let mut v = vec![1, 1, 1];
+    match size {
+        Ok(size) => {
+            if size <= 3 {
+            } else {
+                let mut i = 3;
+                while i < size {
+                    v.push(v[i - 3] + v[i - 2] + v[i - 1]);
+                    i = i + 1;
+                }
+            }
+            println!("Values: {v:?}");
+            Ok(())
+        }
+        Err(message) => {
+            Err(TribonacciError(error_msg))
+        }
+    }
 }
