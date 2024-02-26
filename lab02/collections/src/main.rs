0fb37e762bb15b676abfb99608df29ade62d49f3
Benji Schwartz <benji.schwartz2013@gmail.com>
2024-02-26 11:48:26 +1100

    Finished data analysis task

--- lab02/collections/src/main.rs
+++ lab02/collections/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::collections::{HashMap, LinkedList, VecDeque};
 
 const MAX_ITER: i32 = 300000;
 
@@ -9,11 +9,18 @@ fn main() {
     // VecDeque
     vec_deque_operations();
 
-    // TODO: your code here, for linked list insertions
+    // LinkedList
+    linked_list_operations();
 
-    // TODO: your code here, for hashmap insertions
+    // HashMap
+    hash_map_operations();
 
     // TODO: your text explanation to the questions in the spec
+    // Expectations (before starting):
+    // I expect that a Linked List insertion will be significantly slower than vec/vecdeque
+    // because it required O(n) for insert/delete, whereas vec/vecdeque is O(1) amortized.
+    // I expect that hashmap will have a similar performance to the first two since it is O(1)
+    // insert/delete.
 }
 
 /// measure the insertion and removal
@@ -56,6 +63,46 @@ fn vec_deque_operations() {
     let time_start = std::time::Instant::now();
     for _ in 0..MAX_ITER {
         vec_deque.pop_front();
+    }
+    let time_end = std::time::Instant::now();
+
+    println!("remove: {:?}", time_end - time_start);
+}
+fn linked_list_operations() {
+    let mut list = LinkedList::new();
+
+    let time_start = std::time::Instant::now();
+    for i in 0..MAX_ITER {
+        list.push_back(i);
+    }
+    let time_end = std::time::Instant::now();
+
+    println!("==== Linked List ====");
+    println!("insert: {:?}", time_end - time_start);
+
+    let time_start = std::time::Instant::now();
+    for _ in 0..MAX_ITER {
+        list.pop_front();
+    }
+    let time_end = std::time::Instant::now();
+
+    println!("remove: {:?}", time_end - time_start);
+}
+fn hash_map_operations() {
+    let mut map = HashMap::new();
+
+    let time_start = std::time::Instant::now();
+    for i in 0..MAX_ITER {
+        map.insert(i, i);
+    }
+    let time_end = std::time::Instant::now();
+
+    println!("==== Hash Map ====");
+    println!("insert: {:?}", time_end - time_start);
+
+    let time_start = std::time::Instant::now();
+    for i in 0..MAX_ITER {
+        map.remove(&i);
     }
     let time_end = std::time::Instant::now();
 
--- lab02/data_analysis/src/main.rs
+++ lab02/data_analysis/src/main.rs
@@ -1,5 +1,65 @@
+use std::collections::{HashMap, HashSet};
+use csv::{ReaderBuilder};
+use serde::{Deserialize};
+
+#[derive(Deserialize, Debug, Clone)]
+struct Enrolment {
+    course: String,
+    z_id: String,
+    name: String,
+    program: String,
+    plan: String,
+    wam: f32,
+    session: String,
+    dob: i32,
+    sex: String
+}
 const ENROLMENTS_PATH: &str = "enrolments.psv";
 
 fn main() {
-    println!("Hello, world!");
+
+    let mut rdr = ReaderBuilder::new()
+        .delimiter(b'|')
+        .has_headers(false)
+        .from_path(ENROLMENTS_PATH).unwrap();
+
+    let mut enrolment_vec = Vec::new();
+    for result in rdr.records() {
+        let enrolment: Enrolment = result.unwrap().deserialize(None).unwrap();
+        enrolment_vec.push(enrolment);
+    }
+
+    let students = filter_unique(enrolment_vec.clone());
+    let course_map = count_course(enrolment_vec.clone());
+    let most_common_course = course_map.iter().max_by_key(|entry| entry.1).unwrap();
+    let least_common_course = course_map.iter().min_by_key(|entry| entry.1).unwrap();
+    let average_wam = calculate_average(students.clone());
+
+
+    println!("Number of students: {}", students.len());
+    println!("Most common course: {} with {} students", most_common_course.0, most_common_course.1);
+    println!("Least common course: {} with {} students", least_common_course.0, least_common_course.1);
+    println!("Average WAM: {:.2}", average_wam);
+}
+
+fn filter_unique(vec: Vec<Enrolment>) -> Vec<(String, String)> {
+    vec.into_iter()
+        .map(|enrolment| (enrolment.z_id, enrolment.wam.to_string()))
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect()
+}
+
+fn count_course(vec: Vec<Enrolment>) -> HashMap<String, i32> {
+    vec.into_iter()
+        .map(|enrolment| enrolment.course)
+        .fold(HashMap::new(), |mut acc, c| {
+            *acc.entry(c).or_insert(0) += 1;
+            acc
+        })
+}
+
+fn calculate_average(vec: Vec<(String, String)>) -> f32 {
+    let len = vec.len() as f32;
+    vec.iter().map(|student| student.1.parse::<f32>().unwrap()).sum::<f32>() / len
 }
