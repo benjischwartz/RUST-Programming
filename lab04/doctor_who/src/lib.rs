4b7f6127c08f442f43cd26c2a6e01afabc0b0d15
Benji Schwartz <benji.schwartz2013@gmail.com>
2024-03-08 15:49:29 +1100

    finished doctor_who

--- lab04/doctor_who/src/lib.rs
+++ lab04/doctor_who/src/lib.rs
@@ -1,9 +1,29 @@
-const DEFAULT_SHIFT: i32 = 5;
-const UPPERCASE_A: i32 = 65;
-const LOWERCASE_A: i32 = 97;
-const ALPHABET_SIZE: i32 = 26;
+//! Encrypts a message using the Caesar cypher, with an optional shift value.
+//! Provides the function caesar_shift.
 
-fn caesar_shift(shift_by: Option<i32>, lines: Vec<String>) -> Vec<String> {
+/// Default shift is 5 if none provided.
+pub const DEFAULT_SHIFT: i32 = 5;
+/// ASCII value of 'A'
+pub const UPPERCASE_A: i32 = 65;
+/// ASCII value of 'a'
+pub const LOWERCASE_A: i32 = 97;
+/// Number of characters in the english alphabet
+pub const ALPHABET_SIZE: i32 = 26;
+
+
+/// Reads in a single command-line argument, which is the number
+/// of positions to shift each letter (i32).
+/// If there is no command-line argument, or if the argument is
+/// not a number, the program should use a default shift of 5.
+/// # Examples
+/// ```
+/// # use doctor_who::caesar_shift;
+/// let vec = vec![String::from("hello!")];
+/// assert_eq!(caesar_shift(None, vec), vec!["mjqqt!"]);
+/// let vec = vec![String::from("world")];
+/// assert_eq!(caesar_shift(Some(1), vec), vec!["xpsme"]);
+/// ```
+pub fn caesar_shift(shift_by: Option<i32>, lines: Vec<String>) -> Vec<String> {
     let shift_number = shift_by.unwrap_or(DEFAULT_SHIFT);
     
     // no idea what this is doing? Ask the forums and/or 
@@ -13,7 +33,6 @@ fn caesar_shift(shift_by: Option<i32>, lines: Vec<String>) -> Vec<String> {
         .map(|line| shift(shift_number, line.to_string()))
         .collect()
 }
-
 fn shift(shift_by: i32, line: String) -> String {
     let mut result: Vec<char> = Vec::new();
 
