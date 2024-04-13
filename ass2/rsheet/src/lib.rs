cdef7d706592b8ef4ed9b938d893b4472b68d580
Benji Schwartz <benji.schwartz2013@gmail.com>
2024-04-13 16:55:39 +1000

    debug unwrap bug

--- ass2/rsheet/src/lib.rs
+++ ass2/rsheet/src/lib.rs
@@ -24,7 +24,7 @@ where
     loop {
         let (mut recv, mut send) = match manager.accept_new_connection() {
             Ok(connection) => connection,
-            Err(_) => continue
+            Err(_) => return Ok(())
         };
         let cells = cells_original.clone();
         let thread = thread::spawn( move || {
